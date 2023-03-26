144f6d91b38192fcc258462237ec18d6e0df1813
Leandro Oliveira <leandrohenriqueco@hotmail.com>
2023-03-26 11:24:40 -0400

    initial commit

---
+++ DogBreeds/DogBreeds/DogBreedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogBreedsApp.swift
+//  DogBreeds
+//
+//  Created by Leandro Carvalho on 26/03/23.
+//
+
+import SwiftUI
+
+@main
+struct DogBreedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
